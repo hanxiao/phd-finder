9c0071fb02860b4ab75c897cb01697e5ed588e75
Han Xiao <han.xiao@zalando.de>
2016-12-16 10:40:52 +0100

    switch the translate api traffic

--- src/main/java/MSTranslator.java
+++ src/main/java/MSTranslator.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -20,9 +21,16 @@ class MSTranslator {
     }
 
     private void doAuth() {
-        Translate.setClientId("phd-gmail-account");
-        Translate.setClientSecret("LJPP1Nc+GPd60MvdHujdXFKnYHo0yd+lrIT6VYfcpUU=");
-
+        Random r = new Random();
+        if (r.nextDouble() > 0.5) {
+            // login with artex_xh
+            Translate.setClientId("phd-gmail-account");
+            Translate.setClientSecret("LJPP1Nc+GPd60MvdHujdXFKnYHo0yd+lrIT6VYfcpUU=");
+        } else {
+            // login with hanxiao@live
+            Translate.setClientId("phd-finder");
+            Translate.setClientSecret("scBucjkN5FtVMVj4ET2WcjgBsi8FECJyDg/omVJJR1Q=");
+        }
         try {
             String translatedText = Translate.execute("laptop is good", Language.AUTO_DETECT, Language
                     .CHINESE_SIMPLIFIED);
@@ -30,8 +38,6 @@ class MSTranslator {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-//        Translate.setClientId("phd-finder");
-//        Translate.setClientSecret("scBucjkN5FtVMVj4ET2WcjgBsi8FECJyDg/omVJJR1Q=");
 //        try {
 //            LOG.warn("First translate API cridential failed!");
 //            String translatedText = Translate.execute("Bonjour le monde", Language.AUTO_DETECT, Language.ENGLISH);
